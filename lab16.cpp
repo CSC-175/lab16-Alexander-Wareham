175e48a0326663fb8dcbdb7be39bfc1e1a55fa0f
Alexander Wareham <awareham@umich.edu>
2024-11-13 14:48:24 -0500

    done

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,11 @@
 // Code to implement the gcf function goes here
+
+int gcf(int a, int b) {
+    int t;
+    while (b != 0) {
+        t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
